af1de11825c4fbf2ebd6adfdf326074244ce16db
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-09-27 11:56:36 -0700

    sensors

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,14 @@
-// https://developer.scrypted.app/#getting-started
-// package.json contains the metadata (name, interfaces) about this device
-// under the "scrypted" key.
-import { OnOff, ScryptedDeviceBase } from '@scrypted/sdk';
-import axios from 'axios';
+import { ScryptedInterfaceProperty, AirQuality, AirQualitySensor, NOXSensor, PM10Sensor, ScryptedDeviceBase, VOCSensor } from '@scrypted/sdk';
 
-console.log('Hello World. This will create a virtual OnOff device.');
-// OnOff is a simple binary switch. See "interfaces"  in package.json
-// to add support for more capabilities, like Brightness or Lock.
-
-class TypescriptLight extends ScryptedDeviceBase implements OnOff {
-    constructor(nativeId?: string) {
+class TestSensor extends ScryptedDeviceBase implements VOCSensor, NOXSensor, AirQualitySensor, PM10Sensor {
+    constructor(nativeId: string) {
         super(nativeId);
-        this.on = this.on || false;
-    }
-    async turnOff() {
-        this.console.log('turnOff was called!');
-        this.on = false;
-    }
-    async turnOn() {
-        // set a breakpoint here.
-        this.console.log('turnOn was called!');
 
-        this.console.log("Let's pretend to perform a web request on an API that would turn on a light.");
-        const ip = await axios.get('http://jsonip.com');
-        this.console.log(`my ip: ${ip.data.ip}`);
-
-        this.on = true;
+        this.vocDensity = 33;
+        this.noxDensity = 44;
+        this.airQuality = AirQuality.Excellent;
+        this.pm10Density = 55;
     }
 }
 
-export default TypescriptLight;
+export default TestSensor;
